cd06fb9b33c2decbbf983f681cf756918ef745f4
Romeo13card <romeo_13card_fan@outlook.com>
2026-07-27 21:17:37 +0300

    fog: BringUP @ProjectCiRCLE-ROM

--- circle_fog.mk
+++ circle_fog.mk
@@ -8,14 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CiRCLE stuff.
+$(call inherit-product, vendor/circle/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+CIRCLE_BUILDTYPE := UNOFFICIAL
+WITH_GMS := false
+CIRCLE_MAINTAINER := romeo_13card
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := circle_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
